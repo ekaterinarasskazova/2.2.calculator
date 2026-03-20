95bf84d5198c65c3da06bbf39e138956cc6ae4c1
FrontendCourseMP <rasskazovaekaterina20@gmail.com>
2026-03-21 02:15:08 +0300

    форма

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
     <meta charset="UTF-8">
     <title>Калькулятор</title>
     <link rel="stylesheet" href="style.css">
+    <body>
+        <div class="calculator">
+            <form method="POST" action="calculate.php">
+        </div>
+
+    </body>
 </head>
